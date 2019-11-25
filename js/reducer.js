d66fb13f8c61e5dbdd0de5a9a06178c6fa733966
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-11-25 00:12:11 +0000

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,12 +1,3 @@
-function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
-  }
-}
-
 let state = {count: 0};
 
 function changeState(state, action){
