6b5136c2897e93bb1fb3a25990e9c99eeadffab5
stabassum <sumu1993@gmail.com>
2020-11-22 10:48:50 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,13 @@ function changeState(state, action){
   }
 }
 
+function dispatch(action){
+  state = changeState(state, action)
+  return state
+}
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
+// => {count: 1}
