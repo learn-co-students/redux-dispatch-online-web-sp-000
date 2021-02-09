f91c82c41983cca3440e6f2f920ced6afb8ea4df
Emily Gann <aspentree79@gmail.com>
2021-02-09 08:49:30 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,15 +1,15 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
+function changeState(state = { count: 0 }, action){
+  
   switch (action.type) {
+    
     case 'INCREASE_COUNT':
       return {count: state.count + 1}
-    default:
+    
+      default:
       return state;
   }
-}
-function render() {
-  document.body.textContent = state.count
 }
 
 function dispatch(action){
@@ -17,8 +17,8 @@ function dispatch(action){
   render()
 }
 
-// let state = {count: 0}
-// let action = {type: 'INCREASE_COUNT'}
+function render() {
+  document.body.textContent = state.count
+}
 
-// changeState(state, action)
-render()
+dispatch({ type: '@@INIT' })
