2e5c48b84d03d94b082d4c981d41d7e6507d08ad
clincoln12 <chris.lincoln12@gmail.com>
2021-03-23 23:00:13 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -11,3 +11,14 @@ let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
 changeState(state, action)
+
+function render(){
+    document.body.textContent = state.count
+}
+
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
+
+render()
